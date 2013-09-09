0bbaa4f9480e72d12064329d55734d8638980318
zifan.jzf <zifan.jzf@taobao.com>
2013-09-09 16:39:57 +0800

    question

--- b.php
+++ b.php
@@ -4,10 +4,11 @@
 	$dept = $_REQUEST["dept"];
 	$group = $_REQUEST["group"];
 	$phone = $_REQUEST["phone"];
+	$question = $_REQUEST["question"];
 	$keyword_list = array('AND','OR','=','WHERE','SELECT','>','<');
 	echo "Welcome $name !";
 	echo "Your Information has been sent.";
 	$file = fopen("table.txt","a") or exit("Unable to open file!");
-	fputs($file,"$name\t$number\t$dept\t$group\t$phone");
+	fputs($file,"$name\t$number\t$dept\t$phone\t$group\t$question");
 	fputs($file,"\r\n");
 	fclose($file);
