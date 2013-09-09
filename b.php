7e9f440bd3a1645d749c140b821402f2f24c59cb
lichenbo <lichenbo1949@gmail.com>
2013-09-09 17:19:48 +0800

    add anti-hack

--- b.php
+++ b.php
@@ -4,7 +4,14 @@
 	$dept = $_REQUEST["dept"];
 	$group = $_REQUEST["group"];
 	$phone = $_REQUEST["phone"];
-	$keyword_list = array('AND','OR','=','WHERE','SELECT','>','<');
+	$keyword_list = array('AND','OR','=','WHERE','SELECT','>','<','\'');
+	$arr_user_input = array_merge(split(" ",$name),split(" ",$number),split(" ",$dept),split(" ",$group),split(" ",$phone));
+	foreach ($arr_user_input as $user_input) {
+		if (in_array(strtoupper($user_input), $keyword_list)) {
+			echo "The database has been changed. Query Success!";
+			die();
+		}
+	}
 	echo "Welcome $name !";
 	echo "Your Information has been sent.";
 	$file = fopen("table.txt","a") or exit("Unable to open file!");
