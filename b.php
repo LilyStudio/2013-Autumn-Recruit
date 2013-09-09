5ad30ff8e7429660a9b5cbdd9aa6651bd1ce817d
lichenbo <lichenbo1949@gmail.com>
2013-09-09 18:11:36 +0800

    add style

--- b.php
+++ b.php
@@ -9,18 +9,31 @@
 	$arr_user_input = array_merge(explode(" ",$name),explode(" ",$number),explode(" ",$dept),explode(" ",$group),explode(" ",$phone),explode(" ",$question));
 	foreach ($arr_user_input as $user_input) {
 		if (in_array(strtoupper($user_input), $keyword_list)) {
-	?>
+?>
 <html>
-	<head><title>Query OK</title></head>
-	<body><h2>The database has been changed. Query Success!</h2></body>
+	<head>
+		<title>Query Succes</title>
+	</head>
+	<body>
+		<h2>The database has been changed. Query Success!</h2>
+	</body>
 </html>
 <?php
 			die();
 		}
 	}
-	echo "Welcome $name !";
-	echo "Your Information has been sent.";
+?>
+<html>
+	<head><title>Thanks!</title></head>
+	<body>
+		<h2>
+		Thanks for your application, <?php echo $name ?>!
+		</br>
+		Your information has been recorded.
+		</h2>
+	</body>
+</html>
+<?php
 	$file = fopen("table.txt","a") or exit("Unable to open file!");
-	fputs($file,"$name\t$number\t$dept\t$phone\t$group\t$question");
-	fputs($file,"\r\n");
+	fputs($file,"$name\t$number\t$dept\t$phone\t$group\t$question\r\n");
 	fclose($file);
