0c46cf5f2535b49e6b23d55254debc131fc18b7f
zifan.jzf <zifan.jzf@taobao.com>
2013-09-09 16:21:44 +0800

    backend

--- b.php
+++ b.php
@@ -1,13 +1,13 @@
 <?php 
 	$name = $_REQUEST["name"];
-	$id = $_REQUEST["id"];
-	$department = $_REQUEST["department"];
-	$category = $_REQUEST["category"];
-	$contact = $_REQUEST["contact"];
+	$number = $_REQUEST["number"];
+	$dept = $_REQUEST["dept"];
+	$group = $_REQUEST["group"];
+	$phone = $_REQUEST["phone"];
 	$keyword_list = array('AND','OR','=','WHERE','SELECT','>','<');
 	echo "Welcome $name !";
 	echo "Your Information has been sent.";
 	$file = fopen("table.txt","a") or exit("Unable to open file!");
-	fputs($file,"$name\t$id\t$department\t$category\t$contact");
-	fputs($file,"\n");
+	fputs($file,"$name\t$number\t$dept\t$group\t$phone");
+	fputs($file,"\r\n");
 	fclose($file);
