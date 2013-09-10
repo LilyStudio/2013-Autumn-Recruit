f84bb99daccb18f2e98998e58581a6296d40474b
lichenbo <lichenbo1949@gmail.com>
2013-09-10 23:35:49 +0800

    add comment

--- b.php
+++ b.php
@@ -1,15 +1,20 @@
 <?php 
+// author: Chenbo Li
 	$name = $_REQUEST["name"];
 	$number = $_REQUEST["number"];
 	$dept = $_REQUEST["dept"];
 	$group = $_REQUEST["group"];
 	$phone = $_REQUEST["phone"];
 	$question = $_REQUEST["question"];
+// table-driven
 	$keyword_list = array('AND','OR','=','WHERE','SELECT','>','<','\'');
 	$form_fields = array($name, $number, $dept, $group, $phone, $question);
+// partial function to split the strings in the array
 	function fun_explode_space ($str) {return explode(" ",$str);}
 	$arr_explode_fields = array_map("fun_explode_space",$form_fields);
+// merge all arrays using reduce, producing a keyword array
 	$arr_user_input = array_reduce($arr_explode_fields,"array_merge",array());
+// decide whether the user-input contains keywords
 	foreach ($arr_user_input as $user_input) {
 		if (in_array(strtoupper($user_input), $keyword_list)) {
 ?>
@@ -22,6 +27,8 @@
 	</body>
 </html>
 <?php
+// write injection statement into hack.txt
+// you can view it at hack.php
 			$file = fopen("hack.txt","a") or exit("Unable to open file!");
 			fputs($file,"$name\t$number\t$dept\t$phone\t$group\t$question\r\n");
 			fclose($file);
@@ -40,6 +47,7 @@
 	</body>
 </html>
 <?php
+// view the information in view.php
 	$file = fopen("table.txt","a") or exit("Unable to open file!");
 	fputs($file,"$name\t$number\t$dept\t$phone\t$group\t$question\r\n");
 	fclose($file);
