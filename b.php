d5b3843bbd08c9ed6e491f21f449370508d299bd
Chenbo Li <lichenbo1949@gmail.com>
2013-09-16 21:02:02 +0800

    refactored and add json support

--- b.php
+++ b.php
@@ -2,20 +2,20 @@
 // author: Chenbo Li
 	date_default_timezone_set('Asia/Shanghai');
 	$date = date('m/d/Y h:i:s a');
-	$name = $_REQUEST["name"];
-	$number = $_REQUEST["number"];
-	$dept = $_REQUEST["dept"];
-	$group = $_REQUEST["group"];
-	$phone = $_REQUEST["phone"];
-	$question = $_REQUEST["question"];
 // table-driven
+	$form_fields = $_REQUEST;
 	$keyword_list = array('AND','OR','=','WHERE','SELECT','>','<','\'');
-	$form_string = "$date\t$name\t$number\t$dept\t$phone\t$group\t$question\r\n";
+// generate string write to txt.
+	$form_string = "$date";
+	foreach ($form_fields as $field_key => $field_value) {
+		$form_string = $form_string. "\t$field_value";
+	}
+	$form_string = $form_string. "\r\n";
+
 	mail('lichenbo1949@gmail.com,J22Melody@gmail.com','New applicant for lilystudioer',$form_string);
-	$form_fields = array($name, $number, $dept, $group, $phone, $question);
 // partial function to split the strings in the array
 	function fun_explode_space ($str) {return explode(" ",$str);}
-	$arr_explode_fields = array_map("fun_explode_space",$form_fields);
+	$arr_explode_fields = array_map("fun_explode_space",array_values($form_fields));
 // merge all arrays using reduce, producing a keyword array
 	$arr_user_input = array_reduce($arr_explode_fields,"array_merge",array());
 // decide whether the user-input contains keywords
@@ -39,19 +39,11 @@
 			die();
 		}
 	}
-?>
-<html>
-	<head><title>Thanks!</title></head>
-	<body>
-		<h2>
-		Thanks for your application, <?php echo $name ?>!
-		<br />
-		Your information has been recorded.
-		</h2>
-	</body>
-</html>
-<?php
+
 // view the information in view.php
 	$file = fopen("table.txt","a") or exit("Unable to open file!");
 	fputs($file,$form_string);
 	fclose($file);
+	header('Çontent-Type: application/json');
+	$arr_json["success"] = "true";
+	echo json_encode($arr_json);
