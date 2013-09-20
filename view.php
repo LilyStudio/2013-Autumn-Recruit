46efd26bc65e6dee15bb679993645653af0480e8
Chenbo Li <lichenbo1949@gmail.com>
2013-09-20 17:10:40 +0800

    add new function

--- view.php
+++ view.php
@@ -6,17 +6,49 @@
 <tr><th>Time</th><th>Name</th><th>Number</th><th>Dept</th><th>Phone</th><th>Cat</th><th>Experience</th><th>Hobby</th>
 </tr>
 <?php 
+	$number = $_REQUEST['number'];
 	header("Content-Type:text/html;charset=UTF-8");
 	$file = fopen("table.txt","r") or exit("Unable to open file!");
 	while (($line = fgets($file)) !== false ) {
 		$arr_line = explode("\t",$line);
 		echo "<tr>";
-		foreach($arr_line as $cell) {
-			echo "<td>$cell</td>";
+		if (!$number) {
+			foreach($arr_line as $index=>$cell) {
+				if ($index == 1) {
+					echo "<td><a href='view.php?number=$arr_line[2]'>$cell</a></td>";
+				}
+				else {
+					echo "<td>$cell</td>";
+				}
+			}
+		} else {
+			foreach($arr_line as $cell) {
+				if ($arr_line[2] == $number) {
+					echo "<td>$cell</td>";
+				}
+			}	
+		
 		}
 		echo "</tr>";
 	}
+	fclose($file);
 ?>
 </table>
+<?php 
+	if ($number) {
+			if ($files = scandir('$number')) {
+					foreach( $files as $file_name ) {
+						$file = fopen( $file_name, "r") or exit("Unable to open file $file_name!");
+						echo "<h2>$file_name</h2>";
+						while (($line = fgets($file)) !== false) {
+							echo "<p>$line</p>";
+						}
+						fclose($file);
+					}
+			} else {
+					mkdir('$number');
+			}
+	}
+?>
 </body>
 </html>
