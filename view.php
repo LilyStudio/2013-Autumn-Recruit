08ae6ecf308b7a5ffff1469de9a5ce1160eb1299
Chenbo Li <lichenbo1949@gmail.com>
2013-09-22 23:34:52 +0800

    refine view

--- view.php
+++ view.php
@@ -73,10 +73,10 @@
 <form id='formId' action='addinfo.php' action='post'>
 <p>学号:<input type="text" id="number" name="number" value="<?php echo $number; ?>" readonly></input></p>
 <p>你的名字:<input type="text" id="name" name="name" value="<?php echo $_SESSION['name']; ?>" readonly></input></p>
-<p>记录一下吧:<br /><textarea id="content" name="content" rows="18" cols="40"></textarea></p>
+
+<p>记录一下吧:<div id="msg"></div><br /><textarea id="content" name="content" rows="18" cols="40"></textarea></p>
 <p><input type="submit" id="submit" value="保存"/></p>
 </form>
-<div id="msg"></div>
 			<script>
 			$.ajax({
 				type:"POST",
